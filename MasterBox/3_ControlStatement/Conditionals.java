c3b813aa4cf8379272a938d624ab836bd088f01d
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-08-14 23:44:40 +0530

    conditionals code updated

---
+++ MasterBox/3_ControlStatement/Conditionals.java
@@ -0,0 +1,65 @@
+//Control statements
+//There are two types of control statements in java
+// 1. Conditional (if-else,else-if, switch case)
+// 2 . Iteration or loops (for , for each, while, etc)
+// !Note: for syntax please refer handwritten notes
+public class Conditionals {
+
+  public static void main(String[] args) {
+    System.out.println("~~ Conditionals ~~ \n~ if-else statement.");
+    int age = 21;
+    // if else statement
+    if (age >= 18) {
+      System.out.println("You are allowed in party");
+    } else {
+      System.out.println("You are not allowed in party.");
+    }
+    // here else is not mandatory only "if" also can be used
+
+    // else if statement
+    System.out.println("~ else if statement");
+    if (age <= 0) {
+      System.out.println("Please check your age.");
+    } else if (age >= 18) {
+      System.out.println("You are allowed in party");
+    } else {
+      System.out.println("You are not allowed in party.");
+    }
+    //Switch case
+    // in java we can pass int , char and string as arguments in switch case.
+    // break is necessary other wise your loop can go on infinity.
+    System.out.println("~ Switch case");
+    int toyNumber = 2;
+    switch (toyNumber) {
+      case 1:
+        System.out.println("Ballon");
+        break;
+      case 2:
+        System.out.println("Gun");
+        break;
+      case 3:
+        System.out.println("Phone");
+        break;
+      case 4:
+        System.out.println("Bat");
+        break;
+      case 5:
+        System.out.println("Ball");
+        break;
+      default :  // if anything is not answer then this will be printed
+        System.out.println("You got nothing. Have a great day!");  
+    }
+    //Enhance switch
+    // in this switch case method no need of break statement
+    System.out.println("~ Enhance switch case");
+    switch (toyNumber) {
+      case 1->
+        System.out.println("Ballon");
+      case 2,3,4 ->  // this can't be done in normal switch case 
+        System.out.println("You got nothing");
+      case 5 ->
+      System.out.println("Bat");  
+    }
+    // we can use switch case as a expression also
+  }
+}
