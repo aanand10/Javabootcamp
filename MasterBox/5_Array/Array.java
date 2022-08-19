18d97ba16664d434814d93838292cb19c840d689
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-08-19 21:58:58 +0530

    array code files updated
    1 D array info added.

--- MasterBox/5_Array/Array.java
+++ MasterBox/5_Array/Array.java
@@ -7,8 +7,9 @@ public class Array {
 
   public static void main(String[] args) {
     // Creation of array
+    // 1 D array
     System.out.println(
-      "Creation of the array\n ~ syntax : data-type [] arrayName; "
+      "Creation of the array\n ~ Creation of one dimensional array or 1D array \n~ syntax : data-type [] arrayName; "
     ); // for basic syntax please refer handwritten notes in the array folder
     int[] arr = new int[5]; // int : type of data type which array we want to create
     //  [] : take it as it is for 1D array
@@ -26,5 +27,34 @@ public class Array {
     int[] marksFinal = new int[5]; // Declaration + Memory allocation
     //4.
     int[] marksLast = new int[] { 92, 53, 58, 99, 25 }; // this is also valid syntax
+
+    // Method to traverse an array or Accessing array elements they can be accessed by their index
+    System.out.println("Accessing array elements .\n~ By index");
+    // marksNew[0] this will give us 91
+    System.out.println(marksNew[0]); // 91
+    System.out.println(marksNew[1]); // 96
+    System.out.println(marksNew[2]); // 85
+    System.out.println(marksNew[3]); // 20
+    System.out.println(marksNew[4]); // 14
+    // accessing the elements using for loop
+    System.out.println("~ By for loop");
+    System.out.println("Accessing the elements using for loop");
+    for (int i = 0; i < marksNew.length; i++) {
+      System.out.println("Index is " + i + " and element is : " + marksNew[i]);
+    }
+    // accessing the elements using for-each loop or enhanced for loop
+    System.out.println("~ By for-each loop");
+
+    int index = 0;
+    for (int element : marksNew) {
+      System.out.println("Index is " + index + " and element is : " + element);
+      index++;
+    }
+    // !Fact: let's try to print the info of an array
+    System.out.print(
+      "!Fact: let's try to print the info of an array \n This can be done by printing the array name like  System.out.println(marksNew) : "
+    );
+    System.out.println(marksNew);
+    // read more regarding this in notes provided in the folder
   }
 }
