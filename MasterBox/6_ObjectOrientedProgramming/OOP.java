ce631b0b16cf4f534954325ef1cd9d0d81aac00d
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-08-29 23:26:14 +0530

    OOP code file updated

--- MasterBox/6_ObjectOrientedProgramming/OOP.java
+++ MasterBox/6_ObjectOrientedProgramming/OOP.java
@@ -1,7 +1,31 @@
-//Object oriented Programming : this helps to program or code real world problems efficiently. This is used to break complex problems into simpler and smaller objects.
-//
+//! Part 1 :
+//~ Object oriented Programming : this helps to program or code real world problems efficiently. This is used to break complex problems into simpler and smaller objects.
+//~ Classes , Objects and methods
+//~ Classes : These are the blueprint for the other objects.
+//~ Object : These are created by the classes.
+//~ Method : Objects use methods to communicate between them or we can say methods are the block of code which are used to perform specific task.
+
+//* Let's take a real world example. Consider a case of the car.
+// Assume "Car" as a Class and the name of the car "fordAspire" as an object of car class and the activities or actions done by the car are the methods link for starting "start()" method can be used and the values we pass through the method are called as parameters. Which we will see further.
+// In short  : Class = Car
+//             Object = fordAspire
+//             Method = start()
+class Car {
+
+  String color = "White"; // variable or we can say it is an element of class
+  int price = 10; // variable
+
+  public void start() { // Method
+    System.out.println("Car is Starting.");
+  }
+}
 
 public class OOP {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    //For calling or using anything from another class we need to create its object firstly which will work as an reference to that class. By using that object we are able to access stuff from that class.
+    // Creating an object :
+    // ClassName objectName = new ClassName(); // object is created
+    Car fordAspire = new Car(); // Here fordAspire object is created from the class
+  }
 }
