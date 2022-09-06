d5b0a768745a40524ba5d279822eda6776e209c4
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-09-06 09:22:59 +0530

    NestedClass code file updated

--- MasterBox/6_ObjectOrientedProgramming/NestedClass.java
+++ MasterBox/6_ObjectOrientedProgramming/NestedClass.java
@@ -20,6 +20,10 @@ public class NestedClass {
   public static void main(String[] args) {
     // singleton obj = new singleton();  we can't create object directly because default constructor is private. So we have to create it by accessing the method which we have created. Because that method returns an object.
     singleton obj = singleton.getInstnace();
-    singleton obj1 = singleton.getInstnace();
+    //# for instance/ object creation we call constructor [singleton obj = new singleton();] ; but here the default constructor is private so we have call the public method which we have derived in the class, which returns an instance/ object of the class.
+    //# If we are trying to create more reference variable , it is possible to create more reference variable as of they are pointing to the same object.
+    singleton newObj = singleton.getInstnace();
+    //# Here obj and newObj are pointing top the same object which is returned by the getInstance() method.
+    //!Note : singleton obj = new singleton(); here obj is an reference variable which will point to the created object as object is created we call it as object name.
   }
 }
