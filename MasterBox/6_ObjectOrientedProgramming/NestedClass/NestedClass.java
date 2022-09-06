ae35b7a4e0f7cff2ffa92bdc5bb9a5edeadc8c9a
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-09-06 23:52:31 +0530

    Nested class code files updated

---
+++ MasterBox/6_ObjectOrientedProgramming/NestedClass/NestedClass.java
@@ -0,0 +1,27 @@
+package NestedClass;
+
+//************* Inner class or non-static nested class *************
+// In java we can define a class within another class such classes are known as nested / inner class.
+// Elements in the inner class and elements are can be accessed by dot "." operator.
+// class files will be created as :
+// OuterClass.java
+// OuterClass$InnerClass.java
+
+class OuterClass {
+
+  class InnerClass {
+
+    int a = 10;
+  }
+}
+
+public class NestedClass {
+
+  public static void main(String[] args) {
+    OuterClass obj = new OuterClass(); //Outer class obj created
+    OuterClass.InnerClass obj1 = obj.new InnerClass(); // inner class object created
+
+    OuterClass.InnerClass obj2 = new OuterClass().new InnerClass(); // we can simultaneously create objects of the both the classes.
+    System.out.println(obj1.a);
+  }
+}
--- MasterBox/6_ObjectOrientedProgramming/Singletone.java
+++ MasterBox/6_ObjectOrientedProgramming/Singletone.java
@@ -1,15 +1,15 @@
 //~ Singleton class :  a class whose only one instance can be created. More than one instance creation for that class is not allowed.
 //! Class object is also called as class instance.
 
-class singleton {
+class SingletonClass {
 
-  static singleton object = new singleton(); // static object is created. we kept it static due to that we can access it directly.
+  static SingletonClass object = new SingletonClass(); // static object is created. we kept it static due to that we can access it directly.
 
-  private singleton() { // We are making default constructor private due to that another instance/ object of the class should not get created
+  private SingletonClass() { // We are making default constructor private due to that another instance/ object of the class should not get created
     System.out.println("I'm a private constructor.");
   }
 
-  public static singleton getInstnace() { // Creating a public method which will return an instance/ object which is created above. This will allow to create only one object of the class.
+  public static SingletonClass getInstnace() { // Creating a public method which will return an instance/ object which is created above. This will allow to create only one object of the class.
     return object; // for directly calling here we have kept object static
   }
 }
@@ -18,10 +18,10 @@ public class Singletone {
 
   public static void main(String[] args) {
     // singleton obj = new singleton();  we can't create object directly because default constructor is private. So we have to create it by accessing the method which we have created. Because that method returns an object.
-    singleton obj = singleton.getInstnace();
+    SingletonClass obj = SingletonClass.getInstnace();
     //# for instance/ object creation we call constructor [singleton obj = new singleton();] ; but here the default constructor is private so we have call the public method which we have derived in the class, which returns an instance/ object of the class.
     //# If we are trying to create more reference variable , it is possible to create more reference variable as of they are pointing to the same object.
-    singleton newObj = singleton.getInstnace();
+    SingletonClass newObj = SingletonClass.getInstnace();
     //# Here obj and newObj are pointing top the same object which is returned by the getInstance() method.
     // obj   newObj
     // ⬇     ⬇
