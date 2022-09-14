0e46a4b68c168ee5ac058aa6c8a86e57c440361a
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-09-14 23:30:56 +0530

    OOP code file added

---
+++ MasterBox/7_OOP_Terminology/OOP_Terminology.java
@@ -0,0 +1,8 @@
+//! Object Oriented Programing : Let's see what is Object Oriented Programing and what does it means ?
+//~ Object Oriented Programing (OOP) means solving real world problems using lines of code along with some terminologies like abstraction , inheritance , polymorphism inheritance , aggregation, etc.
+//~ OOP helps us solving real world problems more easily than procedural programing.
+//~ Object is an entity which have some states and behavior. e.g.: Car is an object which have
+//~ states : first gear , AC on, headlights on, etc.
+//~ behavior : moving forward, moving back, braking, etc.
+
+public class OOP_Terminology {}
