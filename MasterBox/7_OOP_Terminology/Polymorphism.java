f6abbbd30ee726edfa8cba0ccc746f4730753cc6
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-09-21 22:52:41 +0530

    Polymorphism files updated

---
+++ MasterBox/7_OOP_Terminology/Polymorphism.java
@@ -0,0 +1,5 @@
+//Polymorphism : the word polymorphism contains its meaning poly means many and morphism means may ways.
+public class Polymorphism {
+
+  public static void main(String[] args) {}
+}
