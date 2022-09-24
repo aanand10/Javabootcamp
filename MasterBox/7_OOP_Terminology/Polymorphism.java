fd81531eb766880da90b800bf1124243c58cbf90
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-09-24 10:48:42 +0530

    code files updated

--- MasterBox/7_OOP_Terminology/Polymorphism.java
+++ MasterBox/7_OOP_Terminology/Polymorphism.java
@@ -1,5 +1,6 @@
-//Polymorphism : the word polymorphism contains its meaning poly means many and morphism means may ways.
-// the real life example of this is yourself. as we play many roles at a time because you are a student, you are brother/sister , son/doughter, etc. at the same time this show many roles/ forms of single person.
+//& Polymorphism : the word polymorphism contains its meaning "poly" means many and "morphism" means may ways.
+// The real world example of this is yourself. As you play many roles at a time because you are a student, you are brother/sister , son/doughter, etc. at the same time this show many roles/ forms of single person.
+//
 public class Polymorphism {
 
   public static void main(String[] args) {}
