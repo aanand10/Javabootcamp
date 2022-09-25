12931f8de2d85bba5ddd30677048175c85928b32
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-09-26 00:32:11 +0530

    Update Polymorphism.java

--- MasterBox/7_OOP_Terminology/Polymorphism.java
+++ MasterBox/7_OOP_Terminology/Polymorphism.java
@@ -1,11 +1,16 @@
-import javax.sound.sampled.SourceDataLine;
-import javax.swing.Renderer;
-
 //& Polymorphism : the word polymorphism contains its meaning "poly" means many and "morphism" means may ways.
 // The real world example of this is yourself. As you play many roles at a time because you are a student, you are brother/sister , son/daughter, etc. at the same time this show many roles/ forms of single person.
-//This is achieved by :
-//  1) Method overriding
-//  2) Method overloading
+//~ This is achieved by :
+//  1) Method overloading
+//        Same name but data types, arguments, return types, order can be different.
+//  2) Method overriding
+//        Name ,  arguments, return type, order everything is same but the body of the method is different.
+//  3) Operator overloading
+
+//~ Types of polymorphism :
+// 1. Compile time/static polymorphism  : achieved via method overloading.
+// 2. Runtime/ Dynamic overriding : achieved via method overriding.
+
 //Example:              Shape
 //                 ↙     ↓     ↘
 //            Triangle  Circle  Rectangle
