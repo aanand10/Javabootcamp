4672e9bed9167e0ed2f92d3f06b8a15f1e9b83c4
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-09-25 19:28:05 +0530

    polymorphism files u[pdated

--- MasterBox/7_OOP_Terminology/Polymorphism.java
+++ MasterBox/7_OOP_Terminology/Polymorphism.java
@@ -1,7 +1,40 @@
+import javax.sound.sampled.SourceDataLine;
+import javax.swing.Renderer;
+
 //& Polymorphism : the word polymorphism contains its meaning "poly" means many and "morphism" means may ways.
-// The real world example of this is yourself. As you play many roles at a time because you are a student, you are brother/sister , son/doughter, etc. at the same time this show many roles/ forms of single person.
-//
+// The real world example of this is yourself. As you play many roles at a time because you are a student, you are brother/sister , son/daughter, etc. at the same time this show many roles/ forms of single person.
+//This is achieved by :
+//  1) Method overriding
+//  2) Method overloading
+//Example:              Shape
+//                 ↙     ↓     ↘
+//            Triangle  Circle  Rectangle
+//# render() method behaves differently in different classes. Or, we can say render() is polymorphic.
+class Shape {
+
+  public void render() {
+    System.out.println("This a shape class.");
+  }
+}
+
+class Circle {
+
+  public void render() {
+    System.out.println("This is a circle class.");
+  }
+}
+
+class Square {
+
+  public void render() {
+    System.out.println("This is a Square class.");
+  }
+}
+
 public class Polymorphism {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Square so = new Square();
+    so.render();
+  }
 }
