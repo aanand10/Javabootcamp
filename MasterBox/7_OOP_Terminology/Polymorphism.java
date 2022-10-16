2e6640c80d947f2901c88a0ab631e6dd205c73bf
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-10-16 23:59:05 +0530

    code files updated

--- MasterBox/7_OOP_Terminology/Polymorphism.java
+++ MasterBox/7_OOP_Terminology/Polymorphism.java
@@ -14,11 +14,65 @@
 //Example:              Shape
 //                 ↙     ↓     ↘
 //            Triangle  Circle  Rectangle
-//^ render() method behaves differently in different classes. Or, we can say render() is polymorphic.
+//~ render() method behaves differently in different classes. Or, we can say render() is polymorphic.  Which have same name in every class but different action is done in every method.
+
+//! If we are referencing from parent class to the method of child then the method which is being overridden should be present in superclass.
+
+//! Fact : By default every class extends Object class.
+/*  class Shape {
+  void meth1(){}
+  void meth2(){}
+}
+
+class Circle {
+  void meth1(){}
+  void meth2(){}
+  void meth3(){}
+}
+ 
+Here if we try to refer shape to circle (Parent to child)
+Shape obj = new Circle();
+obj.meth1();   //valid
+obj.meth2();   //valid
+obj.meth3();   // invalid because meth3 is not present in Shape class (parent class) that must be in Shape class also.
+
+Circle co = new Circle();
+co.meth3();    //Now this is valid 
+ */
+
+//&  Types of Relationship
+//    ↙             ↘
+//Association     Inheritance
+//HAS-A rel.        IS-A rel.
+
+//*    Association (HAS-A relationship )
+// Has two parts :
+// 1. Aggregation : it shares weak bond
+//
+
 class Shape {
 
+  // render() method
   public void render() {
     System.out.println("This a shape class.");
+  }
+
+  //& Compile time polymorphism/ static polymorphism : method overloading
+  //* Here method overloading is done by creating two same named methods having different parameters
+  public void shapeName(String name) {
+    System.out.println("This shape name is " + name + " .");
+  }
+
+  public void shapeName(String name, int id) {
+    System.out.println("This shape name is " + name + "  and it's id is " + id);
+  }
+
+  // & Runtime/ Dynamic overriding : achieved via method overriding.
+  // Method which is being overridden must be available in super class
+  //! Overriding only happens when inheritance is happening.
+
+  public void NumberOfSides(int side) {
+    System.out.println("~ Number of sides of this shape is  : " + side);
   }
 }
 
@@ -36,10 +90,23 @@ class Square {
   }
 }
 
+class Polygon extends Shape {
+
+  @Override // Java Program to Demonstrate that Annotations these are Not Barely Comments.
+  public void NumberOfSides(int side) {
+    System.out.println("~ Number of sides of this shape is  : " + side);
+  }
+}
+
 public class Polymorphism {
 
   public static void main(String[] args) {
+    Shape shapeObj = new Shape();
     Square so = new Square();
+    System.out.print("~ Calling render() : ");
     so.render();
+    System.out.println("~ Example of method overloading : ");
+    shapeObj.shapeName("Crazy");
+    shapeObj.shapeName("Super crazy ", 10);
   }
 }
